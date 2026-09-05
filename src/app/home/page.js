bbdeb1bba0728da82e76e7cc40402df4731cdbf0
MLazizjon <maxmudjonovlazizjon@gmail.com>
2026-09-05 16:43:06 +0500

    All project files added

---
+++ src/app/home/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+export default function HomePage() {
+  return (
+    <Container>
+      <h1>Home Sahifasi</h1>
+    </Container>
+  );
+}
